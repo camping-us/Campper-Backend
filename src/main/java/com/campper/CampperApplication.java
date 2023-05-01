71d4b5021c268c7d50505843a7e4e2ee47694f3a
수민 <squirmm.be@gmail.com>
2023-05-01 15:48:09 +0900

    :rocket: settings

---
+++ src/main/java/com/campper/CampperApplication.java
@@ -0,0 +1,13 @@
+package com.campper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campper/CampperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
