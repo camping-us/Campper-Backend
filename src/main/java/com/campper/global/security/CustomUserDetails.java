e42c242ccc35e39a7af5c5f9b8af8c04e4c01aef
squirMM <jsm6616@naver.com>
2023-05-03 01:40:36 +0900

    :rocket: feat(auth) 인증 관련 security user 커스텀 #6

--- src/main/java/com/campper/global/security/CustomUserDetails.java
+++ src/main/java/com/campper/global/security/CustomUserDetails.java
@@ -1,4 +1,56 @@
-package com.campper.global.security;/**
-* created by squirMM on {date}
-*/public class CustomUserDetails {
+package com.campper.global.security;
+
+
+import com.campper.domain.users.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+    private final Collection<GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user, Collection<GrantedAuthority> authorities) {
+        this.user = user;
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPwd();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getAuthKey();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !user.isDeleted();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !user.isDeleted();
+    }
 }
