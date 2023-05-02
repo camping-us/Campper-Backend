810ec56aa39af2d05312ca66ef91bed63ac97ca1
squirMM <jsm6616@naver.com>
2023-05-03 01:43:20 +0900

    :rocket: feat(auth) 인증 관련 DTO 생성 #6

--- src/main/java/com/campper/domain/auth/dto/request/PostLoginDto.java
+++ src/main/java/com/campper/domain/auth/dto/request/PostLoginDto.java
@@ -1,4 +1,23 @@
-package com.campper.domain.auth.dto.request;/**
-* created by squirMM on {date}
-*/public class PostLoginDto {
+package com.campper.domain.auth.dto.request;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+public class PostLoginDto {
+    @ApiModelProperty(value = "사용자 아이디", example = "U123456789")
+    @NotNull
+    private String authKey;
+    @ApiModelProperty(value = "사용자 비밀번호", example = "U123456789!")
+    @NotNull
+    private String pwd;
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(authKey, pwd);
+    }
 }
--- src/main/java/com/campper/domain/auth/dto/response/GetJwtDto.java
+++ src/main/java/com/campper/domain/auth/dto/response/GetJwtDto.java
@@ -1,16 +1,21 @@
 package com.campper.domain.auth.dto.response;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class GetAccessTokenDto {
+public class GetJwtDto {
     @ApiModelProperty(notes = "accessToken", required = true)
     private String accessToken;
+    @ApiModelProperty(notes = "refreshToken", required = true)
+    private String refreshToken;
 
-    public GetAccessTokenDto(String accessToken) {
+    @Builder
+    public GetJwtDto(String accessToken, String refreshToken) {
         this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
     }
 }
