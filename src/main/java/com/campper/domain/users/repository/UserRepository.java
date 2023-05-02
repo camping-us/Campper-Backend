430a6d94a0dfdf1e732211a2203023063951c92a
수민 <squirmm.be@gmail.com>
2023-05-02 17:25:18 +0900

    :rocket: feat(user) 사용자 레포지토리 #3

--- src/main/java/com/campper/domain/users/repository/UserRepository.java
+++ src/main/java/com/campper/domain/users/repository/UserRepository.java
@@ -1,7 +1,18 @@
-package com.campper.domain.users.mapper;
+package com.campper.domain.users.repository;
 
+import com.campper.domain.users.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+//@Repository
 @Mapper
-public interface UserMapper {
+public interface UserRepository {
+    boolean existByAuthKey(String authKey);
+    Optional<User> findByAuthKey(String authKey);
+    void save(User user);
+    User updatePwd(User user);
+    User updateProfile(User user);
+    void delete(User user);
 }
