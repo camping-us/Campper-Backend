32c13f118fe0feb8bed14f170cca57b9478a965b
수민 <squirmm.be@gmail.com>
2023-05-02 17:31:03 +0900

    :rocket: feat(conifg) EnumValid 생성 #4

--- src/main/java/com/campper/global/common/valid/EnumValid.java
+++ src/main/java/com/campper/global/common/valid/EnumValid.java
@@ -1,2 +1,21 @@
-package com.campper.global.common.valid;public interface EnumValid {
+package com.campper.global.common.valid;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+/**
+ * EnumValidate 확인을 위한 어노테이션입니다.
+ * */
+@Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = EnumValidValidator.class)
+public @interface EnumValid {
+    String message() default "Invalid Enum";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
+
