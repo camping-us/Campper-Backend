e3afa17373089a32a19dd877e2a3d5f9c2a87a72
수민 <squirmm.be@gmail.com>
2023-05-02 17:24:38 +0900

    :rocket: feat(user) 사용자 dto 생성 #3

--- src/main/java/com/campper/domain/users/dto/request/PutUserPwdDto.java
+++ src/main/java/com/campper/domain/users/dto/request/PutUserPwdDto.java
@@ -1,2 +1,18 @@
-package com.campper.domain.users.dto.request;public class PutUserPwdDto {
+package com.campper.domain.users.dto.request;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+public class PutUserPwdDto {
+    @ApiModelProperty(value = "사용자 기존 비밀번호", example = "U12345!")
+    @NotNull
+    private String pwd;
+    @ApiModelProperty(value = "사용자 새 비밀번호", example = "U12345!")
+    @NotNull
+    private String newPwd;
 }
