fa8f8889b6c283fda8da0672fcfd3d3f844e4fcd
squirMM <jsm6616@naver.com>
2023-05-03 01:48:16 +0900

    :rocket: feat(auth) 스웨거 명명 추가 #6

--- src/main/java/com/campper/domain/auth/AuthController.java
+++ src/main/java/com/campper/domain/auth/AuthController.java
@@ -7,6 +7,7 @@ import com.campper.domain.auth.dto.response.GetJwtDto;
 import com.campper.domain.auth.service.AuthService;
 import com.campper.domain.users.entity.User;
 import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,16 +25,19 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
+    @Operation(summary = "로그인 요청", description = "로그인 요청 API 입니다.")
     public GetJwtDto login(@RequestBody @Valid PostLoginDto postLoginDto) {
         return authService.login(postLoginDto);
     }
 
     @PostMapping("/logout")
+    @Operation(summary = "로그아웃 요청", description = "로그아웃 요청 API 입니다.")
     public void logout(@AuthenticationPrincipal User user) {
         authService.logout(user);
     }
 
     @PostMapping("/reissue")
+    @Operation(summary = "리프레시 토큰 재발행 요청", description = "리프레시 토큰 재발행 요청 API 입니다.")
     public GetJwtDto reissue(@RequestBody @Valid PostJwtDto postJwtDto) {
         return authService.reissue(postJwtDto);
     }
