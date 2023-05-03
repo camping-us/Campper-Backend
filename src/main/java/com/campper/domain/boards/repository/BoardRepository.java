62e33d4db54e43e8390f39fd4e00791bbd80f5a9
한상우 <sktkddn777@naver.com>
2023-05-03 10:08:58 +0900

    :rocket: board mapper 작성

--- src/main/java/com/campper/domain/boards/repository/BoardRepository.java
+++ src/main/java/com/campper/domain/boards/repository/BoardRepository.java
@@ -1,8 +1,26 @@
-package com.campper.domain.boards.mapper;
+package com.campper.domain.boards.repository;
 
+import com.campper.domain.boards.entity.Board;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface BoardMapper {
+public interface BoardRepository {
 
+    // id로 글이 있는지 확인
+    // TODO: boolean existByBoardId(Long id);
+
+    // 글 쓰기
+    int save(Board board);
+
+    // 글 리스트 조회
+    // TODO: 글 페이지 조회
+
+    // 글 상세 조회
+    Board findByBoardId(Long id);
+
+    // 글 수정
+    int update(Board board);
+
+    // 글 삭제
+    void delete(Long id);
 }
