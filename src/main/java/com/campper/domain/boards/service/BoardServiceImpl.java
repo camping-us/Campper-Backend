031b769e7571e6462a82f9755883f9baedb41aab
한상우 <sktkddn777@naver.com>
2023-05-17 17:47:58 +0900

    :rocket: 페이징 처리 기능 완료

--- src/main/java/com/campper/domain/boards/service/BoardServiceImpl.java
+++ src/main/java/com/campper/domain/boards/service/BoardServiceImpl.java
@@ -1,13 +1,16 @@
 package com.campper.domain.boards.service;
 
+import com.campper.domain.boards.dto.request.BoardParameterDto;
 import com.campper.domain.boards.dto.request.PatchBoardDto;
 import com.campper.domain.boards.dto.request.SaveBoardDto;
 import com.campper.domain.boards.dto.response.GetBoardDetailDto;
+import com.campper.domain.boards.dto.response.GetBoardDto;
 import com.campper.domain.boards.entity.Board;
 import com.campper.domain.boards.entity.Image;
 import com.campper.domain.boards.repository.BoardRepository;
 import com.campper.domain.boards.repository.ImageRepository;
 import com.campper.domain.users.entity.User;
+import com.campper.domain.users.repository.UserRepository;
 import com.campper.global.common.error.ErrorCode;
 import com.campper.global.common.error.exception.BadRequestException;
 import com.campper.global.common.error.exception.UnauthorizedException;
@@ -25,6 +28,7 @@ public class BoardServiceImpl implements BoardService{
 
     private final BoardRepository boardRepository;
     private final ImageRepository imageRepository;
+    private final UserRepository userRepository;
 
     @Override
     public GetBoardDetailDto save(SaveBoardDto saveBoardDto, User user) {
@@ -45,7 +49,23 @@ public class BoardServiceImpl implements BoardService{
             imageRepository.save(image);
         }
 
-        return GetBoardDetailDto.fromEntity(board, saveBoardDto.getImages());
+        return GetBoardDetailDto.fromEntity(board, user.getNickName(), saveBoardDto.getImages());
+    }
+
+    @Override
+    public List<GetBoardDto> getBoardList(BoardParameterDto boardParameterDto) {
+        List<GetBoardDto> boards = new ArrayList<>();
+
+        int start = boardParameterDto.getPg() == 0 ? 0 : (boardParameterDto.getPg() - 1) * boardParameterDto.getSpp();
+
+        boardParameterDto.setStart(start);
+        List<Board> boardList = boardRepository.listBoard(boardParameterDto);
+        for (Board board : boardList) {
+            User user = userRepository.findById(board.getUserId());
+            String userName = user.getNickName();
+            boards.add(GetBoardDto.fromEntity(board, userName));
+        }
+        return boards;
     }
 
     @Override
@@ -57,7 +77,9 @@ public class BoardServiceImpl implements BoardService{
         for (Image image : imageList)
             images.add(image.getImageUrl());
 
-        return GetBoardDetailDto.fromEntity(board, images);
+        User user = userRepository.findById(board.getUserId());
+
+        return GetBoardDetailDto.fromEntity(board, user.getNickName(), images);
     }
 
     @Override
@@ -78,9 +100,7 @@ public class BoardServiceImpl implements BoardService{
 
         boardRepository.update(board);
 
-
-
-        return GetBoardDetailDto.fromEntity(board, images);
+        return GetBoardDetailDto.fromEntity(board, user.getNickName(), images);
     }
 
     @Override
