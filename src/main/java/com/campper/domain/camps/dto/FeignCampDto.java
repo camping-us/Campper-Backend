0955c436145d002db2f5c6fc748af25dcc9c5e04
수민 <squirmm.be@gmail.com>
2023-05-17 17:48:12 +0900

    :rocket: feat(camp) Feign 용 디티오 생성 #12

--- src/main/java/com/campper/domain/camps/dto/FeignCampDto.java
+++ src/main/java/com/campper/domain/camps/dto/FeignCampDto.java
@@ -1,2 +1,26 @@
-package com.campper.domain.camps.dto;public class FeignCampDto {
+package com.campper.domain.camps.dto;
+
+import com.campper.domain.camps.entity.Camp;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class FeignCampDto {
+
+    @JsonProperty("lineIntro")
+    private String lineIntro;
+
+    @JsonProperty("homepage")
+    private String homepage;
+
+    public static Camp toEntity(FeignCampDto feignCampDto){
+        return Camp.builder()
+                .lineIntro(feignCampDto.getLineIntro())
+                .homePage(feignCampDto.getHomepage())
+                .build();
+    }
 }
