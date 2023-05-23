97adaa2d4492a13a27713a1a32428fd2049fbc68
한상우 <sktkddn777@naver.com>
2023-05-23 21:26:49 +0900

    :rocket: 도메인에서 BaseEntity 상속

--- src/main/java/com/campper/domain/boards/entity/Board.java
+++ src/main/java/com/campper/domain/boards/entity/Board.java
@@ -1,27 +1,32 @@
 package com.campper.domain.boards.entity;
 
+import com.campper.global.common.entity.BaseEntity;
+import com.campper.global.common.entity.TimeBaseEntity;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
-public class Board {
+public class Board extends BaseEntity {
 
     private Long id;
     private String title;
     private String content;
     private Category category;
-    private int isDeleted;
     private int likeCnt;
     private Long userId;
 
     @Builder
-    public Board(Long id, String title, String content, Category category, int isDeleted, int likeCnt, Long userId) {
+    public Board(Long id, String title, String content, Category category, boolean isDeleted, int likeCnt, Long userId, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.category = category;
         this.userId = userId;
-        this.isDeleted = isDeleted;
         this.likeCnt = likeCnt;
+        super.setDeleted(isDeleted);
+        super.setCreatedAt(createdAt);
+        super.setUpdatedAt(updatedAt);
     }
 }
--- src/main/java/com/campper/domain/boards/entity/Image.java
+++ src/main/java/com/campper/domain/boards/entity/Image.java
@@ -1,19 +1,26 @@
 package com.campper.domain.boards.entity;
 
+import com.campper.global.common.entity.BaseEntity;
+import com.campper.global.common.entity.TimeBaseEntity;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
-public class Image {
+public class Image extends BaseEntity {
 
     private Long id;
     private String imageUrl;
     private Long boardId;
 
     @Builder
-    public Image(Long id, String imageUrl, Long boardId) {
+    public Image(Long id, String imageUrl, Long boardId, boolean isDeleted, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
         this.imageUrl = imageUrl;
         this.boardId = boardId;
+        super.setDeleted(isDeleted);
+        super.setCreatedAt(createdAt);
+        super.setUpdatedAt(updatedAt);
     }
 }
